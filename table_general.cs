f44bdd19b748b8d725a38413cc7486ce7a96645f
Dmitry Obukhov 29250 <Dmitry.Obukhov@sandisk.com>
2016-04-03 12:51:21 -0700

    Added text processor and shell

--- table_general.cs
+++ table_general.cs
@@ -6,12 +6,61 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Reflection;
 
 
 namespace UsefulExtensions
 {
     public static class TableGeneralExtensions
     {
+
+        public static void FromClass<T>(this DataTable source, T classInstance)
+        {
+            source.Clear();
+            foreach (PropertyInfo info in typeof(T).GetProperties())
+            {
+                source.Columns.Add(new DataColumn(info.Name, info.PropertyType));
+            }
+        }
+
+        public static void FromObject<T>(this System.Data.DataTable source, T objectInstance)
+        {
+            source.Clear();
+            foreach (PropertyInfo info in typeof(T).GetProperties())
+            {
+                source.Columns.Add(new DataColumn(info.Name, info.PropertyType));
+            }
+            DataRow row = source.NewRow();
+            foreach (PropertyInfo info in typeof(T).GetProperties())
+            {
+                row[info.Name] = info.GetValue(objectInstance, null);
+            }
+            source.Rows.Add(row);
+        }
+
+
+        public static void FromList<T>(this DataTable source, List<T> list)
+        {
+            source.Clear();
+            foreach (PropertyInfo info in typeof(T).GetProperties())
+            {
+                source.Columns.Add(new DataColumn(info.Name, info.PropertyType));
+            }
+            foreach (T t in list)
+            {
+                DataRow row = source.NewRow();
+                foreach (PropertyInfo info in typeof(T).GetProperties())
+                {
+                    row[info.Name] = info.GetValue(t, null);
+                }
+                source.Rows.Add(row);
+            }
+        }
+
+
+
+
+
 
         public static string ToCSV(this System.Data.DataTable table)
         {
@@ -40,6 +89,21 @@ namespace UsefulExtensions
             return result;
         }
 
+        public static bool IsNumericColumn(this System.Data.DataTable table, string colname)
+        {
+            if (!table.Columns.Contains(colname))
+            {
+                return false;
+            }
+            DataColumn col = table.Columns[colname];
+            if (col == null)
+                return false;
+            // Make this const
+            var numericTypes = new[] {  typeof(Byte), typeof(Decimal), typeof(Double),
+                                        typeof(Int16), typeof(Int32), typeof(Int64), typeof(SByte),
+                                        typeof(Single), typeof(UInt16), typeof(UInt32), typeof(UInt64)};
+            return numericTypes.Contains(col.DataType);
+        }
 
 
 
---
+++ text_processor.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsefulExtensions
+{
+    public class TextProcessor
+    {
+        private List<string> text = new List<string>();
+        public Dictionary<int,string> Bookmarks = new Dictionary<int, string>();
+        public string Header { get; set; }
+        public string Footer { get; set; }
+        public string Offset { get; set; }
+        public bool LineNumbers { get; set; }
+
+        public TextProcessor(string fileName = "")
+        {
+            Clear();
+            if (fileName != string.Empty)
+            {
+                Read(fileName);
+            }
+        }
+
+
+        public void Clear()
+        {
+            text.Clear();
+            Bookmarks.Clear();
+            Header = string.Empty;
+            Footer = string.Empty;
+            Offset = string.Empty;
+            LineNumbers = false;
+        }
+
+
+        public int Read(string fileName)
+        {
+            Clear();
+            text = File.ReadAllLines(fileName).ToList();
+            return text.Count;
+        }
+
+        public int Save(string fileName, bool append = true)
+        {
+            using (StreamWriter writer = new StreamWriter(fileName, true))
+            {
+                foreach (string line in text)
+                {
+                    writer.WriteLine(line);
+                }
+            }
+            return 0;
+        }
+
+        public int SaveFormatted(string fileName, bool append = true)
+        {
+            using (StreamWriter writer = new StreamWriter(fileName, true))
+            {
+                if (Header != string.Empty)
+                {
+                    writer.WriteLine(Header);
+                }
+                foreach (string line in text)
+                {
+                    writer.WriteLine(line);
+                }
+                if (Footer != string.Empty)
+                {
+                    writer.WriteLine(Footer);
+                }
+            }
+            return 0;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return text.Count;
+            }
+        }
+
+
+    }
+}
