062a243348aa6b6a1b849c337ca3591b60a17dc6
Dmitry Obukhov 29250 <Dmitry.Obukhov@sandisk.com>
2016-04-02 17:24:44 -0700

    Added table code

---
+++ table_general.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Data;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace UsefulExtensions
+{
+    public static class TableGeneralExtensions
+    {
+
+        public static string ToCSV(this System.Data.DataTable table)
+        {
+            var result = new StringBuilder();
+            for (int i = 0; i < table.Columns.Count; i++)
+            {
+                result.Append(table.Columns[i].ColumnName);
+                result.Append(i == table.Columns.Count - 1 ? "\n" : ",");
+            }
+
+            foreach (DataRow row in table.Rows)
+            {
+                for (int i = 0; i < table.Columns.Count; i++)
+                {
+                    result.Append(row[i].ToString());
+                    result.Append(i == table.Columns.Count - 1 ? "\n" : ",");
+                }
+            }
+            return result.ToString();
+        }
+
+        public static string WriteCSV(this System.Data.DataTable table, string fileName)
+        {
+            string result = table.ToCSV();
+            System.IO.File.WriteAllText(fileName, result.ToString());
+            return result;
+        }
+
+
+
+
+        public static System.Data.DataTable CloneIntToDouble(this System.Data.DataTable source)
+        {
+            System.Data.DataTable retVal = new System.Data.DataTable();
+            foreach (DataColumn dc in source.Columns)
+            {
+                if (dc.DataType == typeof(int))
+                {
+                    retVal.Columns.Add(dc.ColumnName, typeof(double));
+                }
+                else
+                {
+                    retVal.Columns.Add(dc.ColumnName, dc.DataType);
+                }
+            }
+            foreach (DataRow dr in source.Rows)
+            {
+                retVal.ImportRow(dr);
+            }
+            return retVal;
+        }
+
+        public static System.Data.DataTable CloneAllToString(this System.Data.DataTable source)
+        {
+            System.Data.DataTable retVal = new System.Data.DataTable();
+            foreach (DataColumn dc in source.Columns)
+            {
+                retVal.Columns.Add(dc.ColumnName, typeof(string));
+            }
+            foreach (DataRow dr in source.Rows)
+            {
+                retVal.ImportRow(dr);
+            }
+            return retVal;
+        }
+
+    }
+
+}
---
+++ table_statistics.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace UsefulExtensions
+{
+    public static class TableStatisticalExtensions
+    {
+
+        public static Dictionary<string, int> CountColumnValues(this System.Data.DataTable source, string column)
+        {
+            Dictionary<string, int> retVal = new Dictionary<string, int>();
+            foreach (DataRow row in source.Rows)
+            {
+                string key = row[column].ToString();
+                if (retVal.Keys.Contains(key))
+                {
+                    retVal[key]++;
+                }
+                else
+                {
+                    retVal.Add(key, 1);
+                }
+            }
+            return retVal;
+        }
+
+
+        public static Dictionary<string, int> CountColumnValues(this System.Data.DataTable source, string column, params string[] expectedValues)
+        {
+            Dictionary<string, int> retVal = CountColumnValues(source, column);
+            foreach (string templateStr in expectedValues)
+            {
+                if (!retVal.Keys.Contains(templateStr))
+                {
+                    retVal.Add(templateStr, 0);
+                }
+            }
+            return retVal;
+        }
+
+        public static int CountRows(this System.Data.DataTable source, string selector)
+        {
+            DataRow[] selection = source.Select(selector);
+            return selection.Count();
+        }
+
+
+    }
+
+}
