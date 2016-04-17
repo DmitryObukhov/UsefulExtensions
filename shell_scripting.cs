476a8b674616cf160fbf17c7ef56bce56b21d0ea
Dmitry Obukhov 29250 <Dmitry.Obukhov@sandisk.com>
2016-04-17 13:56:59 -0700

    Added - DeleteAll to delete all wildcard files in a directory - SleepControl

--- shell_scripting.cs
+++ shell_scripting.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace UsefulExtensions
 {
@@ -84,5 +85,47 @@ namespace UsefulExtensions
             }
         }
 
+        public void DeleteAll(string rootPath, string mask)
+        {
+            // Delete *.mask files
+            Array.ForEach(Directory.GetFiles(rootPath, mask), delegate (string path) { System.IO.File.Delete(path); });
+        }
+
+
     }
+
+    public static class SleepControl
+    {
+        [FlagsAttribute]
+        private enum EXECUTION_STATE : uint
+        {
+            ES_SYSTEM_REQUIRED = 0x00000001,
+            ES_DISPLAY_REQUIRED = 0x00000002,
+            // Legacy flag, should not be used. 
+            // ES_USER_PRESENT   = 0x00000004, 
+            ES_AWAYMODE_REQUIRED = 0x00000040,
+            ES_CONTINUOUS = 0x80000000,
+        }
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+
+        public static void PreventSleep()
+        {
+            if (SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS
+                | EXECUTION_STATE.ES_DISPLAY_REQUIRED
+                | EXECUTION_STATE.ES_SYSTEM_REQUIRED
+                | EXECUTION_STATE.ES_AWAYMODE_REQUIRED) == 0) //Away mode for Windows >= Vista
+                SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS
+                    | EXECUTION_STATE.ES_DISPLAY_REQUIRED
+                    | EXECUTION_STATE.ES_SYSTEM_REQUIRED); //Windows < Vista, forget away mode
+        }
+
+        public static void EnableSleep()
+        {
+            SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS);
+        }
+
+    }
+
 }
